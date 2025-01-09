1c14995291c0146e76c73602c5358e3d1bdccc16
Qingchen_Jia <879484952@qq.com>
2025-01-09 16:43:05 +0800

    Create constant classes to replace various string variables

---
+++ src/main/java/edu/qingchenjia/heimacomments/common/Constant.java
@@ -0,0 +1,13 @@
+package edu.qingchenjia.heimacomments.common;
+
+public class Constant {
+    public static final String USER_DEFAULT_NICKNAME = "user_";
+
+    public static final String REDIS_LOGIN_CODE_KEY = "login:code:";
+
+    public static final long REDIS_LOGIN_CODE_TTL = 2L;
+
+    public static final String REDIS_LOGIN_TOKEN_KEY = "login:token:";
+
+    public static final long REDIS_LOGIN_TOKEN_TTL = 30L;
+}
