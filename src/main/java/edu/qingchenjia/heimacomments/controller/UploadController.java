802c1eaf6d8eff4c85e46c9a09f3d6a1ebffaa0d
Qingchen_Jia <879484952@qq.com>
2025-01-12 14:22:16 +0800

    Implement the API for deleting images from blog

--- src/main/java/edu/qingchenjia/heimacomments/controller/UploadController.java
+++ src/main/java/edu/qingchenjia/heimacomments/controller/UploadController.java
@@ -4,10 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.crypto.digest.DigestUtil;
 import edu.qingchenjia.heimacomments.common.Constant;
 import edu.qingchenjia.heimacomments.common.R;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -53,5 +50,28 @@ public class UploadController {
 
         // 返回成功响应，包含保存的图片名称
         return R.ok(imgName);
+    }
+
+    /**
+     * 删除博客中的图片
+     * 此方法通过接收图片名称，尝试从指定的路径中删除该图片
+     * 主要用于清理不再需要的博客配图，以节省服务器存储空间
+     *
+     * @param imageName 要删除的图片名称
+     * @return 返回删除操作的结果，如果删除成功则返回R.ok()，否则返回R.fail("错误的文件名称")
+     */
+    @GetMapping("/blog/delete")
+    public R<?> deleteImage(@RequestParam("name") String imageName) {
+        // 根据图片名称构建File对象，用于后续的删除操作
+        File image = new File(Constant.IMAGE_PATH, imageName);
+
+        // 检查构建的File对象是否为目录，如果是，则返回失败响应
+        if (FileUtil.isDirectory(image)) {
+            return R.fail("错误的文件名称");
+        }
+
+        // 尝试删除文件，删除成功后返回成功响应
+        FileUtil.del(image);
+        return R.ok();
     }
 }
